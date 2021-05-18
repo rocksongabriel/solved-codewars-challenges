74eeb4a80799e669cc56b2208f69fa0a44687f94
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-18 03:06:33 +0000

    Solve categorize new member kata using python

---
+++ python/categorize-new-member/categorize-new-member.py
@@ -0,0 +1,7 @@
+def open_or_senior(data):
+    return ["Senior" if person[0] >= 55 and person[1] > 7 else "Open" for person in data]
+    
+
+
+print(
+open_or_senior([[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]))
